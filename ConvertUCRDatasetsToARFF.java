edb7a329ee16b917141079baf2f98a2b0114f768
ResidentBiscuit <jared.ready1@gmail.com>
2015-02-28 20:17:00 -0600

    Initial commit

---
+++ ConvertUCRDatasetsToARFF.java
@@ -0,0 +1,16 @@
+/** This class converts all UCR time series datasets from their Matlab format to
+*	ARFF format in order to be used within Weka.
+*	
+*	@author Jared Ready
+*/
+
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.MatlabLoader;
+ 
+import java.io.File;
+
+public class ConvertUCRDatasetsToARFF
+{
+
+}
